49bdaea0af781e053ec7ac2f8dae1a0d353f6fe4
mathgama <matheus_gama@live.com>
2021-12-12 23:17:13 -0300

    feat: day06 p2

--- src/day06/day06.js
+++ src/day06/day06.js
@@ -3,7 +3,7 @@ import { readInputString } from '../utils.js'
 const input = readInputString('./src/day06/input.txt')
 
 const partOne = () => {  
-  let fishes = input.shift().split(',').map(number => parseInt(number))
+  let fishes = input[0].split(',').map(number => parseInt(number))
 
   for (let i = 1; i <= 80; i++) {
     let startingLength = fishes.length
@@ -14,8 +14,6 @@ const partOne = () => {
       }
       else fishes[j]--
     }
-
-    //console.log('Day ' + i + ': ' + fishes)
   }
 
   const result = fishes.length
@@ -24,4 +22,29 @@ const partOne = () => {
   console.log('Result:', result)
 }
 
+const partTwo = () => {  
+  const fishes = input[0].split(',').map(number => parseInt(number))
+  const timers = new Array(10).fill(0)
+
+  fishes.forEach(fish => timers[fish]++)
+
+  for (let i = 1; i <= 256; i++) {
+    for (let j = 0; j < timers.length; j++) {
+      if (j == 0) {
+        timers[9] = timers[0] // new fish
+        timers[7] += timers[0] // reset current fish
+      }
+      
+      timers[j] = timers[j + 1]
+    }
+  }
+
+  timers[9] = 0
+  const result = timers.reduce((accum, curr) => accum += curr)
+
+  console.log('-- Part two --')
+  console.log('Result:', result)
+}
+
 partOne()
+partTwo()
