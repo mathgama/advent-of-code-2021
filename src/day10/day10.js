2dfc0d5d8b40faa9f02f267337e854badf96376b
mathgama <matheus_gama@live.com>
2021-12-17 16:33:41 -0300

    feat: day10 p2

--- src/day10/day10.js
+++ src/day10/day10.js
@@ -2,7 +2,7 @@ import { readInputString } from '../utils.js'
 
 const input = readInputString('./src/day10/input.txt')
 
-const partOne = () => {
+const processLine = (line) => {
   const regex = new RegExp('[([{<]')
   const tagMatch = {
     ')': '(',
@@ -11,29 +11,32 @@ const partOne = () => {
     '>': '<',
   }
 
-  let result = 0
-  const illegalCharacters = []
+  const stack = []
+  const illegalCharacters=[]
 
-  input.forEach(line => {
-    const stack = []
+  while(line) {
+    const char = line.slice(0,1)
+    line = line.slice(1)
 
-    while(line) {
-      const char = line.slice(0,1)
-      line = line.slice(1)
-
-      if (regex.test(char)) // chunk openning
-        stack.push(char)
-      else { // chunk closing
-        if (stack.pop() != tagMatch[char]) {
-          illegalCharacters.push(char)
-          return
-        }
+    if (regex.test(char)) // chunk openning
+      stack.push(char)
+    else { // chunk closing
+      if (stack.pop() != tagMatch[char]) {
+        return [stack, char]
       }
     }
-  })
+  }
 
-  illegalCharacters.forEach(char => {
-    switch (char) {
+  return [stack, '']
+}
+
+const partOne = () => {
+  let result = 0
+
+  input.forEach(line => {
+    const [stack, illegalChar] = processLine(line)
+
+    switch (illegalChar) {
       case ')':
         result += 3
         break
@@ -53,4 +56,44 @@ const partOne = () => {
   console.log('Result:', result)
 }
 
+const partTwo = () => {
+  let result = 0
+  const lineScores = []
+
+  input.forEach(line => {
+    let score = 0
+
+    const [stack, illegalChar] = processLine(line)
+
+    if (illegalChar) return
+
+    while (stack.length > 0) {
+      score *= 5
+      switch (stack.pop()) {
+        case '(':
+          score += 1
+          break
+        case '[':
+          score += 2
+          break
+        case '{':
+          score += 3
+          break
+        case '<':
+          score += 4
+          break
+      }
+    }
+
+    lineScores.push(score)
+  })
+
+  lineScores.sort((a, b) => a - b)
+  result = lineScores[Math.floor(lineScores.length / 2)]
+
+  console.log('-- Part two --')
+  console.log('Result:', result)
+}
+
 partOne()
+partTwo()
