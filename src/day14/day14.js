d64e184a18000be037f685aa605b8d929440e879
mathgama <matheus_gama@live.com>
2021-12-29 23:31:01 -0300

    feat: day14 p2

--- src/day14/day14.js
+++ src/day14/day14.js
@@ -25,12 +25,13 @@ const countCharacters = (template) => {
 }
 
 const processInput = () => {
-  const template = originalInput.shift()
+  const input = [...originalInput]
+  const template = input.shift()
   const rules = []
   
-  originalInput.shift() // remove empty line
+  input.shift() // remove empty line
 
-  originalInput.forEach(line => {
+  input.forEach(line => {
     const [pair, insertion] = line.split(' -> ')
     rules[pair] = insertion
   })
@@ -58,4 +59,58 @@ const partOne = () => {
   console.log('Result:', max - min)
 }
 
+const addCounter = (counterMap, key, times=1) => {
+  const currentValue = counterMap.get(key) ?? 0
+  counterMap.set(key, currentValue + times)
+}
+
+const partTwo = () => {
+  let [template, rules] = processInput()
+  let pairCounter = new Map()
+  let letterCounter = new Map()
+
+  for (let i = 0; i < template.length; i++) {
+    addCounter(letterCounter, template[i])
+
+    if (i == template.length - 1) break
+
+    const pair = template[i] + template[i+1]
+    addCounter(pairCounter, pair)
+  }
+
+  for (let i = 0; i < 40; i++) {
+    let newPairCounter = new Map()
+
+    for (const [pair, counter] of pairCounter) {
+      const insertion = rules[pair]
+
+      if (insertion) {
+        const leftPair = pair.substring(0, 1) + insertion
+        const rightPair = insertion + pair.substring(1, 2)
+
+        addCounter(newPairCounter, leftPair, counter)
+        addCounter(newPairCounter, rightPair, counter)
+
+        addCounter(letterCounter, insertion, counter)
+      } else {
+        addCounter(newPairCounter, pair, counter)
+      }
+    }
+
+    pairCounter = new Map(newPairCounter)
+  }
+
+  let max = 0
+  let min = Number.MAX_SAFE_INTEGER
+
+  for (const counter of letterCounter.values()) {
+    max = counter > max ? counter : max
+    min = counter < min ? counter : min
+  }
+
+  console.log('-- Part two --')
+  console.log('Result:', max - min)
+}
+
 partOne()
+partTwo()
