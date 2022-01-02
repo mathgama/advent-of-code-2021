8192521738a0659a2a07b459721a75442fa64a15
mathgama <matheus_gama@live.com>
2022-01-02 20:16:43 -0300

    feat: day16 p1

--- src/day16/day16.js
+++ src/day16/day16.js
@@ -3,34 +3,53 @@ import { readInputString } from '../utils.js'
 const originalInput = readInputString('./src/day16/input.txt')
 
 const hex2bin = (hex) => {
-  return parseInt(hex, 16).toString(2)
+  return hex.split('').reduce(
+    (acc, curr) => acc += parseInt(curr, 16).toString(2).padStart(4, '0'), 
+    ''
+  )
+}
+
+const getVersionSum = (bin, packetStartIndex) => {
+  const i = packetStartIndex
+  let j
+
+  if (i > bin.length || bin.substring(i).replace('0', '') == '') return 0
+
+  const versionNumber = parseInt(bin.substring(i, i+3), 2)
+  const packetType = bin.substring(i+3, i+6)
+
+  if (packetType == '100') {
+    j = i + 6
+    while (true) {
+      const keepReading = bin.substring(j, j+1) == '1' ? true : false
+      j += 5
+      if (!keepReading) break
+    }
+  }
+  else {
+    const lengthType = bin.substring(i+6, i+7)
+
+    j = i + 7
+
+    if (lengthType == '0') {
+      const totalLength = parseInt(bin.substring(i+7, i+22), 2)
+      j += 15
+    } else {
+      const numberSubPackets = parseInt(bin.substring(i+7, i+18), 2)
+      j += 11
+    }
+  }
+
+  return versionNumber + getVersionSum(bin, j)
 }
 
 const partOne = () => {
-  const bin = hex2bin(originalInput)
+  const bin = hex2bin(originalInput.shift())
 
-  const packetType = bin.substring(i, i+3)
+  const result = getVersionSum (bin, 0)
 
-  if (packetType == '100') {
-    i += 3
-
-    while (true) {
-      const keepReading = bin.substring(i, i+1) == '1' ? true : false
-      i += 5
-      if (!keepReading) break
-    }
-  } else {
-    i += 6
-    const lengthType = bin.substring(i, i+1)
-
-    i += 1
-
-    if (lengthType = '0') {
-      const totalLength = parseInt(bin.substring(i, i+15), 2)
-    } else {
-      const numberSubPackets = parseInt(bin.substring(i, i+11), 2)
-    }
-  }
+  console.log('-- Part one --')
+  console.log('Result:', result)
 }
 
 partOne()
