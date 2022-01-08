bfff4741a7e62daadcb5fbf8788ec31209baca50
mathgama <matheus_gama@live.com>
2022-01-08 15:13:06 -0300

    feat: day20 p1

---
+++ src/day20/day20.js
@@ -0,0 +1,97 @@
+import { readInputString } from '../utils.js'
+
+const originalInput = readInputString('./src/day20/input.txt')
+
+const initializeGrid = (size) => {
+  const grid = new Array(size + 6)
+  for (let i = 0; i < grid.length; i++) grid[i] = new Array(size + 6).fill('.')
+  //grid.forEach(cell => cell = new Array(size + 4).fill('.'))
+  return grid
+}
+
+const processInput = (input) => {
+  const algorithm = input.shift()
+  input.shift()
+
+  let grid
+
+  input.forEach((line, index) => {
+    if (!grid) grid = initializeGrid(line.length)
+
+    for (let i = 0; i < line.length; i++) {
+      grid[index+3][i+3] = line[i]
+    }
+  })
+
+  return [algorithm, grid]
+}
+
+const getSurroundingCoordinates = (i, j, gridLength) => {
+  const square = []
+
+  if (i == 0 || j == 0 || i == (gridLength - 1) || j == (gridLength - 1)) return
+
+  square.push({x: i-1, y: j-1})
+  square.push({x: i-1, y: j})
+  square.push({x: i-1, y: j+1})
+
+  square.push({x: i, y: j-1})
+  square.push({x: i, y: j})
+  square.push({x: i, y: j+1})
+
+  square.push({x: i+1, y: j-1})
+  square.push({x: i+1, y: j})
+  square.push({x: i+1, y: j+1})
+
+  return square
+}
+
+const imageEnhancement = (algorithm, grid) => {
+  const result = initializeGrid(100)
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      const square = getSurroundingCoordinates(i, j, grid.length) ?? []
+      
+      let binary = ''
+
+      if (square.length > 0) {
+        square.forEach(coord => binary += grid[coord.x][coord.y] == '#' ? '1' : '0')
+        result[i][j] = algorithm[parseInt(binary, 2)]
+      } else {
+        result[i][j] = grid[i][j] == '.' ? '#' : '.'
+      }
+    }
+  }
+
+  return result
+}
+
+const countPixels = (grid) => {
+  let pixels = 0
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      pixels += grid[i][j] == '#' ? 1 : 0
+    }
+  }
+
+  return pixels
+}
+
+const partOne = () => {
+  let [algorithm, grid] = processInput([...originalInput])
+
+  for (let i = 0; i < 2; i++) {
+    grid = imageEnhancement(algorithm, grid)
+  }
+
+  //for (let i = 0; i < grid.length; i++) console.log(grid[i].join(''))
+
+  const result = countPixels(grid)
+
+  console.log('-- Part one --')
+  console.log('Result:', result)
+}
+
+partOne()
