5899154bfbdba01293a9c4e196e23a7c30202609
mathgama <matheus_gama@live.com>
2022-01-14 17:30:18 -0300

    fix: day23 p1

--- src/day23/day23.js
+++ src/day23/day23.js
@@ -1,8 +1,9 @@
-import { readInputString } from './utils.js'
+import { readInputString } from '../utils.js'
 
 const originalInput = readInputString('./src/day23/input.txt')
 
 const doors = [2, 4, 6, 8]
+const amphipods = ['A', 'B', 'C', 'D']
 
 const targetRoom = {
   'A': doors[0],
@@ -18,14 +19,16 @@ const cost = {
   'D': 1000,
 }
 
-const calculateCost = (hallway, origin, destiny) => {
+const calculateCost = (state, origin, destiny) => {
   let moves = 0
 
+  const pod = doors.includes(origin) ? state[origin][0] : state[origin]
+
   if (doors.includes(origin)) // moving out of a room
-    moves += hallway[origin].length === 1 ? 2 : 1 // cost to get to the door
+    moves += state[origin].length === 1 ? 2 : 1 // cost to get to the door
 
   if (doors.includes(destiny)) // moving into a room
-    moves += hallway[destiny].length === 1 ? 1 : 2
+    moves += state[destiny].length === 1 ? 1 : 2
 
   const diff = destiny - origin
 
@@ -33,57 +36,66 @@ const calculateCost = (hallway, origin, destiny) => {
     diff > 0 ? i++ : i--
 
     // if not empty and not a door, path is blocked
-    if (hallway[origin + i] !== '.' && !doors.includes(origin + i)) return 0
+    if (state[origin + i] !== '.' && !doors.includes(origin + i)) return 0
 
     moves++
   }
 
-  return moves * cost[hallway[origin]]
+  return moves * cost[pod]
 }
 
-const getPossibleMoves = (hallway) => {
+const getPossibleMoves = (state) => {
   const possibleMoves = []
+  let newState
 
   // room to hallway or to another room
   for (let i = 0; i < doors.length; i++) {
     const doorIndex = doors[i]
-    if (hallway[doorIndex].length === 0) continue
+    if (state[doorIndex].length === 0) continue
 
-    for (let j = 0; j < hallway.length; j++) {
+    for (let j = 0; j < state.length; j++) {
       if (j == doorIndex) continue // do not move to the same place
 
       if (
-          hallway[j] == "." ||
-          (doors.includes(j) && !hallway[j].some(el => el != hallway[j][0]))
+          state[j] == "." ||
+          (doors.includes(j) && !state[j].some(el => el != state[j][0]))
          ) {
-        const cost = calculateCost(hallway, doorIndex, j);
+        const cost = calculateCost(state, doorIndex, j);
         if (cost == 0) continue
+
+        newState = JSON.parse(JSON.stringify(state))
+        const pod = newState[doorIndex].shift()
+        newState[doorIndex].unshift('.')
+        doors.includes(j) ? newState[j].shift(pod) : newState[j] = pod
         
         possibleMoves.push({
-          from: doorIndex,
-          to: j,
-          cost
+          state: newState,
+          cost: cost
         })
       }
     }
   }
   
   // hallway to room
-  for (let i = 0; i < hallway.length; i++) {
-    const value = hallway[i]
+  for (let i = 0; i < state.length; i++) {
+    const value = state[i]
     
     if (value == '.' || Array.isArray(value)) continue
     
     const doorIndex = targetRoom[value]
 
-    if (!hallway[doorIndex].some(el => el != value)) {
-      const cost = calculateCost(hallway, i, doorIndex)
+    if (!state[doorIndex].some(el => el != value)) {
+      const cost = calculateCost(state, i, doorIndex)
       if (cost == 0) continue
+
+      newState = JSON.parse(JSON.stringify(state))
+      const pod = newState[i]
+      newState[i] = '.'
+      newState[doorIndex].unshift(pod)
       
       possibleMoves.push({
-        from: i,
-        to: doorIndex,
-        cost
+        state: newState,
+        cost: cost
       })
     }
   }
@@ -91,8 +103,39 @@ const getPossibleMoves = (hallway) => {
   return possibleMoves
 }
 
-const shortestPath = (hallway) => {
-  
+const isFinalState = (state) => {
+  console.log(state.flat(2).join(''))
+  for (let i = 0; i < doors.length; i++) {
+    const door = doors[i]
+    if (state[door].some(el => el != amphipods[i]))
+      return false
+  }
+  return true
+}
+
+const shortestPath = (initialHallway) => {
+  let queue = []
+  const seen = new Set()
+
+  queue.push({ state: initialHallway, cost: 0 })
+
+  while(queue.length > 0) {
+    queue.sort((a, b) => b.cost - a.cost)
+
+    const {state, cost} = queue.shift()
+    const stateString = state.flat(2).join('')
+
+    if (!seen.has(stateString)) {
+      seen.add(stateString)
+
+      if(isFinalState(state))
+        return cost
+      else {
+        const possibleMoves = getPossibleMoves(state)
+        queue = queue.concat(possibleMoves)
+      }
+    }
+  }
 }
 
 const partOne = () => {
@@ -112,6 +155,7 @@ const partOne = () => {
   })
 
   const result = shortestPath(hallway)
+  console.log(result)
 }
 
 partOne()
